b3747365d953fbf5771d8db614a5673ad3aca938
Dhanuja <dhanujashaji4554@gmail.com>
2023-12-05 08:52:49 +0530

    commited

--- social/models.py
+++ social/models.py
@@ -2,18 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 from django.utils import timezone
+from django.db.models.signals import post_save
 
 # Create your models here.
 
 class UserProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
-    address=models.CharField(max_length=200)
-    phone=models.CharField(max_length=200)
+    address=models.CharField(max_length=200,null=True)
+    phone=models.CharField(max_length=200,null=True)
     profile_pic=models.ImageField(upload_to="profilepics",null=True,blank=True)
-    dob=models.DateField()
-    bio=models.CharField(max_length=200)
-    following=models.ManyToManyField("self",related_name="followed_by",symmetrical=False)
-    block=models.ManyToManyField("self",related_name="blocked",symmetrical=False)
+    dob=models.DateField(null=True)
+    bio=models.CharField(max_length=200,null=True)
+    following=models.ManyToManyField("self",related_name="followed_by",symmetrical=False,null=True)
+    block=models.ManyToManyField("self",related_name="blocked",symmetrical=False,null=True)
 
     def __str__(self):
         return self.user.username
@@ -23,7 +24,7 @@ class Posts(models.Model):
     title=models.CharField(max_length=200)
     post_image=models.ImageField(upload_to="posters",null=True)
     create_date=models.DateField(auto_now_add=True)
-    liked_by=models.ManyToManyField(user,related_name="post_like")
+    liked_by=models.ManyToManyField(User,related_name="post_like")
 
     def __str__(self):
         return self.title
@@ -44,8 +45,19 @@ class Stories(models.Model):
     title=models.CharField(max_length=200)
     post_image=models.ImageField(upload_to="stories",null=True)
     create_date=models.DateField(auto_now_add=True)
-    exp=create_date + timezone.timedelta(days=1)
-    expiry_date=models.DateTimeField(exp)
+    # exp=create_date + timezone.timedelta(days=1)
+    expiry_date=models.DateTimeField()
 
     def __str__(self):
         return self.title
+    
+
+
+def create_profile(sender,created,instance,**kwargs):
+
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+
+post_save.connect(create_profile,sender=User)
--- social/views.py
+++ social/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import View,TemplateView,FormView,CreateView
+from social.forms import RegisterationForm
+from django.urls import reverse
 
-# Create your views here.
+
+class SignUpView(CreateView):
+
+    # def get(self,request,*args,**kwargs):
+    #     form=RegisterationForm()
+    #     return render(request,"register.html",{"form":form})
+    
+    # FormView=> for form display
+# --------------------------------------------
+    template_name="register.html"
+    form_class=RegisterationForm
+
+    def get_success_url(self):
+        return reverse("signup")
+
+    # def post(self,request,*args,**kwargs):
+    #     form=RegisterationForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect("signup")
+    #     else:
+    #         return render(request,"register.html",{"form":form})
+
+# ----------------------------------------
+
+
+# TemplateView => to render a html page
+# class IndexView(TemplateView):
+#     template_name="index.html"
+
+
